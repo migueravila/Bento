7fb58936130387be97de90659a76b97973750b53
Rod <miguelravila@outlook.com>
2020-08-14 13:20:46 -0500

    Fixed comments

--- js/weather.js
+++ js/weather.js
@@ -9,7 +9,7 @@ weather.temperature = {
 };
 
 const KELVIN = 273;
-// You can use your own key for the Weather
+// Use your own key for the Weather, Get it here: https://openweathermap.org/
 const key = 'aa5b0a76dfbf87441928fb3cc32d3d69';
 
 // Set Position function

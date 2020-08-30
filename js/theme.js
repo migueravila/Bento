c2c58d6faaa284189eee5fdcb0e13c33d51d1baf
Rod <miguelravila@outlook.com>
2020-08-30 01:32:40 -0500

    Added Light / Dark mode

--- js/theme.js
+++ js/theme.js
@@ -2,9 +2,30 @@ let darkTheme = localStorage.getItem('darkTheme');
 const themeToggle = document.querySelector('#themeButton');
 
 const enableDark = () => {
-    
+    document.body.classList.add('darktheme');
+    localStorage.setItem('darkTheme', 'enabled');
+    themeToggle.innerHTML = `<i id="bicon" data-feather="moon"></i>`;
+    feather.replace();
+};
+
+const disableDark = () => {
+    document.body.classList.remove('darktheme');
+    localStorage.setItem('darkTheme', null);
+    themeToggle.innerHTML = `<i id="bicon" data-feather="sun"></i>`;
+    feather.replace();
+};
+
+if (darkTheme === 'enabled') {
+    enableDark();
+} else {
+    disableDark();
 }
 
 themeToggle.addEventListener('click', () => {
-    
+    darkTheme = localStorage.getItem('darkTheme');
+    if (darkTheme !== 'enabled') {
+        enableDark();
+    } else {
+        disableDark();
+    }
 });

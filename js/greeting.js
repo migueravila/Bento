72373fa3b53baa0a38def571d87b37fccc375bb9
Eduardo Garcia <luiseduardogarciadepablo@protonmail.com>
2021-03-17 09:05:43 +0100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = 'Eduardo579';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';

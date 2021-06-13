e5ca1fd927f4c6fadbf9439c79bc5173531b1d0d
Nobel <54449465+NPAUDEL@users.noreply.github.com>
2021-06-13 19:56:17 +0545

    Personalized

--- js/greeting.js
+++ js/greeting.js
@@ -3,11 +3,11 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' Nobel';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
+const gree1 = 'Go to Sleep  ';
+const gree2 = 'Good morning  ';
 const gree3 = 'Good afternoon  ';
 const gree4 = 'Good evening,  ';
 const gree5 = 'Good evening,  ';
--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '641bc5bffa709360aabaeb882bfafec2';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 27.717245;
+  let longitude = 85.323959;
 
   getWeather(latitude, longitude);
 }

93de279a0a8025bbf08aa20b04bb9796f9cbd00c
Miguel <migueravila@pm.me>
2021-07-12 14:14:13 -0500

    :art: Updated assets name and package

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,7 +1,1 @@
-const drawCards = () => {
-  for (const card of CONFIG.cards) {
-    let currentCard = document.createElement('a');
-    let currentCardText = document.createElement('p');
-    currentCardText.appendChild(document.createTextNode(card.name));
-  }
-};
+

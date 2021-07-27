c9dd1328598b1413ba5892bd94bebaf66ba6e017
Nobel <54449465+NPAUDEL@users.noreply.github.com>
2021-07-27 20:41:07 +0545

    Revert "Personalized"
    This reverts commit e5ca1fd927f4c6fadbf9439c79bc5173531b1d0d.

--- js/greeting.js
+++ js/greeting.js
@@ -3,11 +3,11 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' Nobel';
+const name = ' John Doe';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep  ';
-const gree2 = 'Good morning  ';
+const gree1 = 'Go to Sleep!  ';
+const gree2 = 'Good morning!  ';
 const gree3 = 'Good afternoon  ';
 const gree4 = 'Good evening,  ';
 const gree5 = 'Good evening,  ';

1754470d56e722c5851dba110b43ced5e716e65b
Simeon Widdis <sawiddis@gmail.com>
2021-08-15 00:43:53 -0600

    Fix logic for night greeting

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -16,7 +16,7 @@ const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
 const gree4 = `${CONFIG.greetingEvening}\xa0`;
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
+if (hour >= 23 || hour < 5) {
   document.getElementById('greetings').innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
   document.getElementById('greetings').innerText = gree2 + name;

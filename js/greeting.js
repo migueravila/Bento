bdc49c00233c1004bafd46efc8d89dc923da5eab
livegren8 <livegren8@gmail.com>
2021-08-20 19:44:28 +0000

    view apps on all views

--- js/greeting.js
+++ js/greeting.js
@@ -2,7 +2,7 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change the greetings and your name
-var name = 'Nathaniel';
+var name = '';
 var lateTxt = 'Go to Sleep! ';
 var morningTxt = 'Good morning! ';
 var afterTxt = 'Good afternoon ';

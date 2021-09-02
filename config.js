614ffbfb8e57ce565ae84fe3fff0db112e7a5348
Shreyan <88901329+shreyan-haldankar@users.noreply.github.com>
2021-09-02 17:20:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'Shreyan',
   imageBackground: false,
   openInNewTab: true,
-  twelveHourFormat: false,
+  twelveHourFormat: true,
 
   // Greetings
   greetingMorning: 'Good morning!',
@@ -23,11 +23,11 @@ const CONFIG = {
   greetingNight: 'Go to Sleep!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
+  weatherKey: '2c69e12c4c4733806658bed63e495c54',
   weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
   weatherUnit: 'C',
-  weatherLatitude: '37.774929',
-  weatherLongitude: '-122.419418',
+  weatherLatitude: '18.989401',
+  weatherLongitude: '73.117516',
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
@@ -39,25 +39,25 @@ const CONFIG = {
       id: '1',
       name: 'Github',
       icon: 'github',
-      link: 'https://github.com/',
+      link: 'https://github.com/shreyan-haldankar',
     },
     {
       id: '2',
       name: 'Mail',
       icon: 'mail',
-      link: 'https://mail.protonmail.com/',
+      link: 'mailto:shreyanhaldankar@gmail.com',
     },
     {
       id: '3',
       name: 'Todoist',
       icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
+      link: 'https://coinmarketcap.com/',
     },
     {
       id: '4',
       name: 'Calendar',
       icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      link: 'https://todoist.com/',
     },
     {
       id: '5',
@@ -69,7 +69,7 @@ const CONFIG = {
       id: '6',
       name: 'Odysee',
       icon: 'youtube',
-      link: 'https://odysee.com/',
+      link: 'https://www.youtube.com',
     },
   ],
 
@@ -85,38 +85,39 @@ const CONFIG = {
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Coding',
+        link: 'https://open.spotify.com/playlist/37i9dQZF1DX5trt9i14X7j?si=b3b46c36f71641b3',
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Workout',
+        link: 'https://open.spotify.com/playlist/37i9dQZF1DX70RN3TfWWJh?si=ad47388c1883494e',
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Mix 1',
+        link: 'https://open.spotify.com/playlist/37i9dQZF1E35q4H6Z4bph8?si=e00a2dcdd75a4405',
       },
       {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Mix 2',
+        link: 'https://open.spotify.com/playlist/37i9dQZF1E39JJ1NSJczhF?si=2960f5824e7e4b59',
       },
+      
     ],
     secondList: [
       {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
+        name: 'Frontend Mentor',
+        link: 'https://www.frontendmentor.io//',
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
+        name: 'Freecodecamp',
+        link: 'https://www.freecodecamp.org/',
       },
       {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
+        name: 'Full Stack Open',
+        link: 'hhttps://fullstackopen.com/',
       },
       {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
+        name: 'HackerRank',
+        link: 'https://www.hackerrank.com/',
       },
     ],
   },

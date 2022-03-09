fd367f9aabf1393d1264600e10bc21b7197ad6f3
oscarpax <memantere@gmail.com>
2022-03-08 19:42:42 -0800

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
   bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+  weatherKey: '9021a883a49491c21adef99c2f18147f', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
+  weatherUnit: 'F', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: '33.659',
+  defaultLongitude: '-117.998',
 
   // Autochange
   autoChangeTheme: true,

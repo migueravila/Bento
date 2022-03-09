9426a02641d9a980787e4bbf500e2048863c0f7d
oscarpax <memantere@gmail.com>
2022-03-08 22:18:22 -0800

    Update config.js

--- config.js
+++ config.js
@@ -38,11 +38,11 @@ const CONFIG = {
   // Autochange
   autoChangeTheme: true,
 
-  // Autochabge by OS
+  // Autochange by OS
   changeThemeByOS: true,
 
   // Autochange by hour options (24hrs format, string must be in: hh:mm)
-  changeThemeByHour: false,
+  changeThemeByHour: true,
   hourDarkThemeActive: '18:30',
   hourDarkThemeInactive: '07:00',
 

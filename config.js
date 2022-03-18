a37b526c62170aa974686a21a02baa858f914b11
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2022-03-18 18:56:13 -0400

    add weather

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+  weatherKey: 'b7ce85f5fedbe72b9966ab00dca8ec6d', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi

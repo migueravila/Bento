8848d7ae0089e3720f3eec2941567b18e6c477a7
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2022-03-28 01:22:21 -0400

    patch update

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -17,6 +17,7 @@ var tempUnit = CONFIG.weatherUnit;
 const KELVIN = 273.15;
 const key = `${CONFIG.weatherKey}`;
 
+setPosition(); // call first, setINterval doesnt trigger when first reached
 setInterval(function() {
 	setPosition();
 }, 120000) // run once every two minutes

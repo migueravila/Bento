94c14fe77df54c1e6ffe1cb69d23276c54fea19d
BilalAG7 <90452010+BilalAG7@users.noreply.github.com>
2022-04-29 17:36:13 +0000

    Update config.js

--- config.js
+++ config.js
@@ -11,40 +11,22 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
-	imageBackground: false,
-	openInNewTab: true,
-	twelveHourFormat: false,
+  	name: 'Bilal',
+ 	imageBackground: false,
+  	openInNewTab: false,
 
-	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+  	// Greetings
+  	greetingMorning: 'Good morning,',
+  	greetingAfternoon: 'Good afternoon,',
+  	greetingEvening: 'Good evening,',
+  	greetingNight: 'Good evening,',
 
-	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
-
-	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
-
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
-
-	// Autochange
-	autoChangeTheme: true,
-
-	// Autochange by OS
-	changeThemeByOS: true,
-
-	// Autochange by hour options (24hrs format, string must be in: hh:mm)
-	changeThemeByHour: false,
-	hourDarkThemeActive: '18:30',
-	hourDarkThemeInactive: '07:00',
+  	// Weather
+  	weatherKey: 'f7b9795af021d5b7e584a0f4fae9ce59',
+  	weatherIcons: 'OneDark',
+  	weatherUnit: 'C',
+  	weatherLatitude: '25.204849',
+  	weatherLongitude: '55.270782',
 
 	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
 	// ├┴┐│ │ │  │ │ ││││└─┐
@@ -52,179 +34,96 @@ const CONFIG = {
 
 	firstButtonsContainer: [
 		{
-			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
-		},
-		{
-			id: '2',
-			name: 'Mail',
-			icon: 'mail',
-			link: 'https://mail.protonmail.com/',
-		},
-		{
-			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
-		},
-		{
-			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
-		},
-		{
-			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
-		},
-		{
-			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
-		},
-	],
-
-	secondButtonsContainer: [
-		{
-			id: '1',
-			name: 'Music',
-			icon: 'headphones',
-			link: 'https://open.spotify.com',
-		},
-		{
-			id: '2',
-			name: 'twitter',
-			icon: 'twitter',
-			link: 'https://twitter.com/',
-		},
-		{
-			id: '3',
-			name: 'bot',
-			icon: 'bot',
-			link: 'https://discord.com/app',
-		},
-		{
-			id: '4',
-			name: 'Amazon',
-			icon: 'shopping-bag',
-			link: 'https://amazon.com/',
-		},
-		{
-			id: '5',
-			name: 'Hashnode',
-			icon: 'pen-tool',
-			link: 'https://hashnode.com/',
-		},
-		{
-			id: '6',
-			name: 'Figma',
-			icon: 'figma',
-			link: 'https://figma.com/',
-		},
-	],
-
+      id: '1',
+      name: 'Twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/home',
+    },
+    {
+      id: '2',
+      name: 'Anime List',
+      icon: 'list',
+      link: 'https://anilist.co/user/BilalAG/animelist',
+    },
+    {
+      id: '3',
+      name: 'Mail',
+      icon: 'mail',
+      link: 'https://mail.google.com/mail/u/0/#inbox',
+    },
+    {
+      id: '4',
+      name: 'YouTube',
+      icon: 'youtube',
+      link: 'https://youtube.com/',
+    },
+    {
+      id: '5',
+      name: 'Netflix',
+      icon: 'film',
+      link: 'https://www.netflix.com/browse',
+    },
+    {
+      id: '6',
+      name: 'MangaReader',
+      icon: 'book-open',
+      link: 'https://proxy.cubari.moe/#/saved',
+    },
+  ],
+	
 	// ┬  ┬┌─┐┌┬┐┌─┐
 	// │  │└─┐ │ └─┐
 	// ┴─┘┴└─┘ ┴ └─┘
 
-	// First Links Container
-	firstlistsContainer: [
-		{
-			icon: 'music',
-			id: '1',
-			links: [
-				{
-					name: 'Inspirational',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				},
-				{
-					name: 'Classic',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				},
-				{
-					name: 'Oldies',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				},
-				{
-					name: 'Rock',
-					link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-				},
-			],
-		},
-		{
-			icon: 'coffee',
-			id: '2',
-			links: [
-				{
-					name: 'Linkedin',
-					link: 'https://www.linkedin.com',
-				},
-				{
-					name: 'Dribbble',
-					link: 'https://www.dribbble.com',
-				},
-				{
-					name: 'Trello',
-					link: 'https://www.trello.com',
-				},
-				{
-					name: 'Slack',
-					link: 'https://www.slack.com',
-				},
-			],
-		},
-	],
+	 //Icons
+  firstListIcon: 'home',
+  secondListIcon: 'hash',
 
-	// Second Links Container
-	secondListsContainer: [
-		{
-			icon: 'binary',
-			id: '1',
-			links: [
-				{
-					name: 'Spotify',
-					link: 'https://www.spotify.com',
-				},
-				{
-					name: 'Reddit',
-					link: 'https://www.reddit.com',
-				},
-				{
-					name: 'Hashnode',
-					link: 'https://www.hashnode.com',
-				},
-				{
-					name: 'Pocket',
-					link: 'https://www.pocket.com',
-				},
-			],
-		},
-		{
-			icon: 'github',
-			id: '2',
-			links: [
-				{
-					name: 'Front',
-					link: 'https://www.reddit.com/r/Frontend/',
-				},
-				{
-					name: 'Rust',
-					link: 'https://www.reddit.com/r/rust/',
-				},
-				{
-					name: 'Go',
-					link: 'https://www.reddit.com/r/golang/',
-				},
-				{
-					name: 'Repos',
-					link: 'https://github.com/migueravila',
-				},
-			],
-		},
-	],
+  // Links
+  lists: {
+    firstList: [
+      {
+        name: 'Official Mail',
+        link: 'https://mail.google.com/mail/u/1/#inbox',
+      },
+      {
+        name: 'Reddit',
+        link: 'https://reddit.com',
+      },
+      {
+        name: 'Router Settings',
+        link: 'http://192.168.0.1/info/Login.html',
+      },
+      {
+        name: 'World Table Tennis',
+        link: 'https://worldtabletennis.com/home',
+      },
+      {
+        name: '1337x',
+        link: 'https://1337xto.to/',
+      },
+    ],
+    secondList: [
+      {
+        name: 'Nyaa',
+        link: 'https://nyaa.si/',
+      },
+      {
+        name: "Sea Smoke's Index",
+        link: 'https://docs.google.com/spreadsheets/d/1emW2Zsb0gEtEHiub_YHpazvBd4lL4saxCwyPhbtxXYM/htmlview',
+      },
+      {
+        name: 'GEMS Portal',
+        link: 'https://gems.ae',
+      },
+      {
+        name: 'Fansubber\'s Index',
+        link: 'https://index.fansubcar.tel/',
+      },
+      {
+        name: 'Anime Filler List',
+        link: 'https://www.animefillerlist.com/',
+      },
+    ],
+  },
 };

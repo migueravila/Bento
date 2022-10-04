9803b5af93335e62013aa67a2fe5676441af2761
Rohit Vaidyula <vaidyular@gmail.com>
2022-10-04 06:22:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -26,11 +26,11 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'fb59af7cb0e4e06affe7e039c8e7e876', // Write here your API Key
+	weatherKey: '5dd2c0882b2215ad65a37013949f63c2', // Write here your API Key
 	weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'F', // 'F', 'C'
-	weatherLatitude: '29.760427',
-  	weatherLongitude: '-95.369804',
+	weatherLatitude: '29.760',
+  	weatherLongitude: '-95.369',
 	language: 'en',// More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
@@ -55,39 +55,27 @@ const CONFIG = {
 	firstButtonsContainer: [
 		{
 			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
+			name: 'Texas A&M Canvas',
+			icon: 'graduation-cap',
+			link: 'https://canvas.tamu.edu/',
 		},
 		{
 			id: '2',
-			name: 'Mail',
+			name: 'GMail',
 			icon: 'mail',
-			link: 'https://mail.protonmail.com/',
+			link: 'https://mail.google.com/mail/u/0/#inbox',
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'Habitica',
+			icon: 'list-checks',
+			link: 'https://habitica.com/',
 		},
 		{
 			id: '4',
 			name: 'Calendar',
 			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
-		},
-		{
-			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
-		},
-		{
-			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
+			link: 'https://calendar.google.com/calendar/u/0/r?pli=1',
 		},
 	],
 

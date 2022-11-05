8f42570abd4ddc34a60255357f67bc006d5720c8
frans <franswinata6@gmail.com>
2022-11-05 17:44:03 +0700

    Input update

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,5 +1,16 @@
+const modal = document.getElementById("modalOverlay");
 let inputName;
 
+modal.innerHTML = `
+      <div class="modalPopup">
+				<div class="modalContent">
+					<h1>Input your name here</h1>
+					<input type="text" id="name" style="border-radius: 25; margin-top: 10px;">
+					<button class="buttonStyle" id="button">Submit</button>
+				</div>
+			</div>`;
+
+// Uncomment the line below if you wanna use localStorage
 // if(localStorage.getItem("name") != null)
 // {
 //     document.getElementById('modalOverlay').style.display = 'none'
@@ -12,7 +23,10 @@ window.onload = function() {
   document.getElementById('button').onclick = () => {
 	  document.getElementById('modalOverlay').style.display = 'none'
 	  inputName = document.getElementById('name').value;
+
+    // Uncomment the line below if you wanna use localStorage
     // localStorage.setItem("name", inputName);
+
     CONFIG.name = inputName;
 	  document.getElementById('greetings').innerText += CONFIG.name;
   }

3ea3ebde6bc1a86e34e9ebf120dc07b3b847078c
your-mom-spec-ops <117489566+your-mom-spec-ops@users.noreply.github.com>
2023-01-15 22:35:41 +0000

    force darktheme?

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
 	changeThemeByOS: false,
 
 	// Autochange by hour options (24hrs format, string must be in: hh:mm)
-	changeThemeByHour: false,
-	hourDarkThemeActive: '18:30',
-	hourDarkThemeInactive: '07:00',
+	changeThemeByHour: true,
+	hourDarkThemeActive: '1:00',
+	hourDarkThemeInactive: '24:00',
 
 	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
 	// ├┴┐│ │ │  │ │ ││││└─┐

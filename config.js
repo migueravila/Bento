35c916c9e48943f09e1a306f2a99d0358d89c535
your-mom-spec-ops <117489566+your-mom-spec-ops@users.noreply.github.com>
2023-01-15 19:59:10 +0000

    i cummed

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ const CONFIG = {
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
 	defaultLatitude: '69',
 	defaultLongitude: '420',
 

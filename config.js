2224aa7ce26c5e03d624614bb1f7e5bc5552b62d
KuroShin7 <157518105+KuroShin7@users.noreply.github.com>
2024-01-23 18:14:10 +0000

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
-	imageBackground: false,
+	name: 'KuroShin',
+	imageBackground: true,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
